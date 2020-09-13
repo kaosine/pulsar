6b6246a3516d244367232792bd441fb3351d4140
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-13 03:56:52 -0500

    parallelize clean

--- script/lib/clean-output-directory.js
+++ script/lib/clean-output-directory.js
@@ -1,9 +1,11 @@
 const fs = require('fs-extra');
+const rimrafDir = require('@lerna/rimraf-dir');
 const CONFIG = require('../config');
 
 module.exports = function() {
   if (fs.existsSync(CONFIG.buildOutputPath)) {
     console.log(`Cleaning ${CONFIG.buildOutputPath}`);
-    fs.removeSync(CONFIG.buildOutputPath);
+    return rimrafDir(CONFIG.buildOutputPath);
   }
+  return Promise.resolve();
 };
